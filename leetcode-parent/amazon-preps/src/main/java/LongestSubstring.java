dab44cd983d8e4184cdffe07a4759a14bdc5f144
Alex <droph88@gmail.com>
2020-09-07 17:52:55 +0300

    Longest substring

---
+++ leetcode-parent/amazon-preps/src/main/java/LongestSubstring.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+/**
+ * Longest Substring Without Repeating Characters
+ *
+ * Given a string s, find the length of the longest substring without repeating characters.
+ */
+public class LongestSubstring {
+    public static int lengthOfLongestSubstring(String str) {
+        var positions = new HashMap<Character, Integer>();
+        var size = str.length();
+        var ans = 0;
+        for(int i = 0, j = 0; j < size; j++) {
+            var currChar = str.charAt(j);
+            if (positions.containsKey(currChar)){
+                i = Math.max(positions.get(currChar), i);
+            }
+            positions.put(currChar, j + 1);
+            ans = Math.max(ans, j - i + 1);
+        }
+        return ans;
+    }
+    //idvedf
+}
---
+++ leetcode-parent/amazon-preps/src/test/java/LongestSubstringTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LongestSubstringTest {
+    private static Stream<Arguments> provideTestParams() {
+        return Stream.of(
+                Arguments.of("abcabcbb", 3),
+                Arguments.of("bbbbb", 1),
+                Arguments.of("pwwkew", 3),
+                Arguments.of("idvedf", 4),
+                Arguments.of("", 0)
+        );
+    }
+
+    @MethodSource("provideTestParams")
+    @ParameterizedTest
+    public void shouldReturnResultAccordingToParams(String string,
+                                                    int count) {
+        assertThat(LongestSubstring.lengthOfLongestSubstring(string))
+                .isEqualTo(count);
+    }
+}
